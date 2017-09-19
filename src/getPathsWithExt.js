0ead5913f155f42ac9edbf0c2c86f715e7c948a6
Ron Gierlach <rongierlach@gmail.com>
2017-09-19 00:54:28 -0500

    getPathsWithExt helper

---
+++ src/getPathsWithExt.js
@@ -0,0 +1,17 @@
+import klaw from 'klaw'
+import { extname } from 'path'
+import filter from 'through2-filter'
+
+const filterByExt = (ext: string) => filter.obj(item => (
+  extname(item.path) === `.${ext}`
+))
+
+export default (ext, dir = __dirname) => (
+  new Promise((resolve, reject) => {
+    const results = []
+    klaw(dir)
+      .pipe(filterByExt(ext))
+      .on('data', item => results.push(item.path))
+      .on('end', () => resolve(results))
+  })
+)
