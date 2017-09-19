8e987ab6974ae834572a44cb728bd9e3ec676895
Ron Gierlach <rongierlach@gmail.com>
2017-09-19 03:07:14 -0500

    some styles

---
+++ example/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'React'
+
+import '../styles/red.css'
+import '../styles/blue.css'
+
+const IndexPage = () => (
+  <div>
+    <p className='blue'>I'm Blue</p>
+  </div>
+)
+
+export default IndexPage
