939aef113324760594efd2538a7397fc43cdeaa0
Ron Gierlach <rongierlach@gmail.com>
2017-09-19 10:00:26 -0500

    alternate promise syntax

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import sanitizeCSS from './sanitizeCSS'
 import getPathsWithExt from './getPathsWithExt'
 import selectAndReplaceInFileFactory from './selectAndReplaceInFileFactory'
 
-const purifyAsync = promisify(purify)
+const purifyAsync = promisify(purify) // eslint-disable-line
 
 exports.onPostBuild = async (_, {purifyOptions = {minify: true}}, cb) => {
   const rootPath = resolve('public')
 
   // get file paths
   const htmlFiles = await getPathsWithExt('html', rootPath)
-  const cssFiles = await getPathsWithExt('css', rootPath)
+  const [jsStyles, compiledStyles] = await getPathsWithExt('css', rootPath) // eslint-disable-line
 
   // purify CSS, remove comments, sourcemaps, and newlines
-  let purifiedCSS = await purifyAsync(htmlFiles, cssFiles, purifyOptions)
+  // let purifiedCSS = await purifyAsync(htmlFiles, cssFiles, purifyOptions)
+  let purifiedCSS = await new Promise((resolve, reject) => {
+    console.log(htmlFiles, compiledStyles, purifyOptions)
+    purify(htmlFiles, compiledStyles, purifyOptions, resolve)
+  })
   purifiedCSS = sanitizeCSS(purifiedCSS)
 
   // replace CSS in html files
